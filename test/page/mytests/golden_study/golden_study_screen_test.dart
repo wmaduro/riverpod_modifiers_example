52ff5f71afd3900bbf1958c672e3b995dba25cb8
Welerson Maduro <welerson.maduro@foodstuffs-si.co.nz>
2022-01-05 22:35:16 +1300

    multiple devices / theme

--- test/page/mytests/golden_study/golden_study_screen_test.dart
+++ test/page/mytests/golden_study/golden_study_screen_test.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:riverpod_modifiers_example/page/mytests/golden_study/golden_study_screen.dart';
 
+final _devicesPhones = [
+  Device.phone.copyWith(name: 'phone_portrait'),
+  Device(name: 'phone_landscape', size: Device.phone.size.flipped),
+  Device.tabletLandscape,
+  Device.tabletPortrait,
+];
 void main() {
   testGoldens('golden: simple red text', (tester) async {
     // await loadAppFonts();
 
-    await tester.pumpWidget(createApp(buildSimpleTextcreen()));
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: _devicesPhones)
+      ..addScenario(widget: createApp(buildSimpleTextcreen()));
+
+    await tester.pumpDeviceBuilder(builder);
+
+    // await tester.pumpWidget(createApp(buildSimpleTextcreen()));
 
     await screenMatchesGolden(tester, 'simple_red_text');
   });
@@ -15,6 +27,7 @@ void main() {
 Widget createApp(Widget child) {
   return MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
     home: Scaffold(body: Center(child: child)),
   );
 }
