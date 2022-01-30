98ed41168a489e7b8051acef23cd66b103702f8c
Welerson Maduro <welerson.maduro@foodstuffs-si.co.nz>
2022-01-31 08:51:19 +1300

    cleaning

--- lib/page/mytests/testing_study/pages/login_widget.dart
+++ lib/page/mytests/testing_study/pages/login_widget.dart
@@ -50,128 +50,141 @@ class _LoginScreenState extends State<_LoginScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            SizedBox(
-              height: 60,
-              width: 200,
-            ),
-
             // Login text Widget
-            Center(
-              child: Container(
-                height: 200,
-                width: 400,
-                alignment: Alignment.center,
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  // textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-
-            SizedBox(
-              height: 60,
-              width: 10,
-            ),
+            _headerLogin(),
 
             // Wrong Password text
-            Visibility(
-              visible: _isVisible,
-              maintainSize: true,
-              maintainAnimation: true,
-              maintainState: true,
-              child: Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  "Wrong credentials entered",
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-            ),
+            _errorMessage(),
 
             // Textfields for username and password fields
-            Container(
-              height: 140,
-              width: 530,
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)), color: Colors.white),
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    onTap: () {
-                      setState(() {
-                        _isVisible = false;
-                      });
-                    },
-                    controller: usernameController, // Controller for Username
-                    decoration: InputDecoration(
-                        border: InputBorder.none, hintText: "Username", contentPadding: EdgeInsets.all(20)),
-                    onEditingComplete: () => FocusScope.of(context).nextFocus(),
-                  ),
-                  Divider(
-                    thickness: 3,
-                  ),
-                  TextFormField(
-                    onTap: () {
-                      setState(() {
-                        _isVisible = false;
-                      });
-                    },
-
-                    controller: passwordController, // Controller for Password
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password",
-                        contentPadding: EdgeInsets.all(20),
-                        // Adding the visibility icon to toggle visibility of the password field
-                        suffixIcon: IconButton(
-                          icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
-                          onPressed: () {
-                            setState(() {
-                              _isObscure = !_isObscure;
-                            });
-                          },
-                        )),
-                    obscureText: _isObscure,
-                  ),
-                ],
-              ),
-            ),
+            _fields(context),
 
             // Submit Button
-            Container(
-              width: 570,
-              height: 70,
-              padding: EdgeInsets.only(top: 20),
-              child: RaisedButton(
-                  color: Colors.pink,
-                  child: Text("Submit", style: TextStyle(color: Colors.white)),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                  onPressed: () {
-                    if (kDebugMode) {
-                      if (usernameController.text == fetchCredentials()[0] &&
-                          passwordController.text == fetchCredentials()[1]) {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => LixoPageTestingStudy()),
-                          (Route<dynamic> route) => false,
-                        );
-                      } else {
-                        setState(() {
-                          _isVisible = true;
-                        });
-                      }
-                    }
-                  }),
-            ),
+            _button(context),
           ],
         ));
+  }
+
+  Container _button(BuildContext context) {
+    return Container(
+      width: 570,
+      height: 70,
+      padding: EdgeInsets.only(top: 20),
+      child: RaisedButton(
+          color: Colors.pink,
+          child: Text("Submit", style: TextStyle(color: Colors.white)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          onPressed: () {
+            if (kDebugMode) {
+              if (usernameController.text == fetchCredentials()[0] &&
+                  passwordController.text == fetchCredentials()[1]) {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LixoPageTestingStudy()),
+                  (Route<dynamic> route) => false,
+                );
+              } else {
+                setState(() {
+                  _isVisible = true;
+                });
+              }
+            }
+          }),
+    );
+  }
+
+  Container _fields(BuildContext context) {
+    return Container(
+      height: 140,
+      width: 530,
+      decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)), color: Colors.white),
+      child: Column(
+        children: <Widget>[
+          _emailField(context),
+          Divider(
+            thickness: 3,
+          ),
+          _passwordField(),
+        ],
+      ),
+    );
+  }
+
+  TextFormField _passwordField() {
+    return TextFormField(
+      onTap: () {
+        setState(() {
+          _isVisible = false;
+        });
+      },
+
+      controller: passwordController, // Controller for Password
+      decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: "Password",
+          contentPadding: EdgeInsets.all(20),
+          // Adding the visibility icon to toggle visibility of the password field
+          suffixIcon: IconButton(
+            icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
+            onPressed: () {
+              setState(() {
+                _isObscure = !_isObscure;
+              });
+            },
+          )),
+      obscureText: _isObscure,
+    );
+  }
+
+  TextFormField _emailField(BuildContext context) {
+    return TextFormField(
+      onTap: () {
+        setState(() {
+          _isVisible = false;
+        });
+      },
+      controller: usernameController, // Controller for Username
+      decoration: InputDecoration(border: InputBorder.none, hintText: "Username", contentPadding: EdgeInsets.all(20)),
+      onEditingComplete: () => FocusScope.of(context).nextFocus(),
+    );
+  }
+
+  Visibility _errorMessage() {
+    return Visibility(
+      visible: _isVisible,
+      maintainSize: true,
+      maintainAnimation: true,
+      maintainState: true,
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.all(10),
+        child: Text(
+          "Wrong credentials entered",
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 10,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Center _headerLogin() {
+    return Center(
+      child: Container(
+        height: 200,
+        width: 400,
+        alignment: Alignment.center,
+        child: Text(
+          "Login",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 34,
+            fontWeight: FontWeight.bold,
+          ),
+          // textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
 }
